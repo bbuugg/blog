3ab8b5ec790b22772de2ce8a8da2db333d70eb61
chengyao <987861463@qq.com>
2022-10-06 13:37:04 +0800

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+echo "Generating posts"
 hexo g
 
+echo 'Adding to git'
 git add -A
+echo 'Added'
+echo 'Committing'
 git commit -m update
+echo 'Committed'
+echo 'Pushing'
 git push
+echo 'Finished'
